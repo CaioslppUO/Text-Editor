6fc8d5a6d161d840b14a18ef60e34f872abf00e1
CaioslppUO <caioslppuo@gmail.com>
2020-04-23 09:49:29 -0300

    V 0.1.3. Adicionada a opção de salvar o que está escrito no editor

--- src/GUI/src/gui/Gui.java
+++ src/GUI/src/gui/Gui.java
@@ -11,13 +11,22 @@ import java.awt.SystemColor;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.beans.PropertyChangeListener;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+import javax.swing.Action;
 import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JDialog;
 import javax.swing.JEditorPane;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JList;
@@ -28,6 +37,7 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JSpinner;
+import javax.swing.KeyStroke;
 import javax.swing.border.BevelBorder;
 import javax.swing.border.EtchedBorder;
 import javax.swing.border.SoftBevelBorder;
@@ -53,6 +63,8 @@ public class Gui implements ActionListener{
 	private JDialog editorConfigFrame;
 	private JSpinner fontSizeSpinner;
 	private JComboBox fontTypeList;
+	
+	private JDialog saveFileFrame;
 
 	public Gui() {
 		this.PaneEditorColor = new Color(51, 51, 51);
@@ -168,6 +180,22 @@ public class Gui implements ActionListener{
 		//Adicionando o botão new ao menu bar
 		menuBar.add(menu);
 		
+		//Botão save
+		menu = new JMenu("Save");
+		menu.setBackground(this.sideAreasColor);
+		menu.setForeground(this.MenuForeGroundColor);
+		
+		//Sub botão save as
+		menuItem = new JMenuItem("Save as");
+		menuItem.setForeground(this.MenuForeGroundColor);
+		menuItem.setBackground(this.sideAreasColor);
+		menuItem.setActionCommand("buttonSaveAsPressed");
+		menuItem.addActionListener(this);
+		menu.add(menuItem);
+		
+		//Adicionando o botão save ao menu bar
+		menuBar.add(menu);
+		
 		//Botão settings
 		menu = new JMenu("Settings");
 		menu.setBackground(this.sideAreasColor);
@@ -199,7 +227,6 @@ public class Gui implements ActionListener{
 	private void launchEditorConfigFrame() {		
 		this.editorConfigFrame = new JDialog();
 		this.editorConfigFrame.getContentPane().setBackground(this.sideAreasColor);
-		this.editorConfigFrame.setLayout(new BorderLayout(0, 0));
 		this.editorConfigFrame.setSize(300, 300);
 		this.editorConfigFrame.setLocationRelativeTo(null);
 		this.editorConfigFrame.setTitle("Editor Settings");
@@ -246,6 +273,50 @@ public class Gui implements ActionListener{
 		
 		this.editorConfigFrame.setVisible(true);
 	}
+	
+	private void saveFile() {
+		String fileSeparator = System.getProperty("file.separator");
+		
+		this.saveFileFrame = new JDialog();
+		this.saveFileFrame.getContentPane().setBackground(this.sideAreasColor);
+		this.saveFileFrame.setSize(600, 600);
+		this.saveFileFrame.setLocationRelativeTo(null);
+		this.saveFileFrame.setTitle("Save File");
+		
+		String contentToSave;
+		contentToSave = this.editorPane.getText();
+		
+		JFileChooser chooseSaveDirectory;
+		chooseSaveDirectory = new JFileChooser();
+		chooseSaveDirectory.setCurrentDirectory(new File("."));
+		chooseSaveDirectory.setDialogTitle("Save to");
+		chooseSaveDirectory.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+		int result = chooseSaveDirectory.showOpenDialog(null);
+		if(result == JFileChooser.APPROVE_OPTION) { //Salvar o arquivo
+			File directory = chooseSaveDirectory.getSelectedFile();
+			String fileName;
+			fileName = JOptionPane.showInputDialog("File Name");
+			File newFile = new File(directory.toString() + fileSeparator + fileName);
+			try {
+				if(newFile.createNewFile()) {
+					FileWriter writer = new FileWriter(directory.toString() + fileSeparator + fileName, false);
+					PrintWriter print_line = new PrintWriter(writer);
+					print_line.printf("%s", contentToSave);
+					print_line.close();
+					JOptionPane.showMessageDialog(null, "File Saved");
+				}else {
+					JOptionPane.showMessageDialog(null, "File Already Exists");
+				}
+			} catch (IOException e) {
+				JOptionPane.showMessageDialog(null, "Error while trying to save the file");
+			}
+		}else { //Não salvar o arquivo	
+		}
+		this.saveFileFrame.add(chooseSaveDirectory);
+		
+		this.saveFileFrame.setVisible(true);
+		this.saveFileFrame.dispose();
+	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
@@ -260,6 +331,8 @@ public class Gui implements ActionListener{
 			this.fontSize = Integer.parseInt(this.fontSizeSpinner.getValue().toString());
 			this.fontType = this.fontTypeList.getSelectedItem().toString();
 			this.editorPane.setFont(new Font(this.fontType, Font.PLAIN, this.fontSize));
+		}else if("buttonSaveAsPressed".equals(e.getActionCommand())) {
+			this.saveFile();
 		}
 	}
 	
