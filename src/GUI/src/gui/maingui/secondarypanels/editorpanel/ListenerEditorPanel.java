5163f09b51e38519f019d124de71f47d3e894a03
CaioslppUO <caioslppuo@gmail.com>
2020-05-04 21:16:07 -0300

    V 0.8.0. Correção de bug: Bug na função Run File.

--- src/GUI/src/gui/maingui/secondarypanels/editorpanel/ListenerEditorPanel.java
+++ src/GUI/src/gui/maingui/secondarypanels/editorpanel/ListenerEditorPanel.java
@@ -18,31 +18,31 @@ public class ListenerEditorPanel implements KeyListener {
     public void keyReleased(KeyEvent e) {
         Gui aux = Gui.getInstance();
         // Executa um programa em python ou em c
-        if (aux.getEditorPane().getEditorPane() != null && e.isControlDown() && e.getKeyChar() != 'r'
+        if (aux.getEditorPane().getEditorPane().isEnabled() && e.isControlDown() && e.getKeyChar() != 'r'
                 && e.getKeyCode() == 82) {
             aux.runSelectedFile();
         }
 
         // Salva o arquivo aberto ao apertar ctrl+s
-        if (aux.getEditorPane().getEditorPane() != null && e.isShiftDown() == false && e.isControlDown()
+        if (aux.getEditorPane().getEditorPane().isEnabled() && e.isShiftDown() == false && e.isControlDown()
                 && e.getKeyChar() != 's' && e.getKeyCode() == 83) {
             aux.getSaveFile().saveFile(true, aux.getCurrentFile(), aux.getEditorPane().getEditorPane().getText());
         }
 
         // Salva como o arquivo ao apertar ctrl+shift+s
-        if (aux.getEditorPane().getEditorPane() != null && e.isShiftDown() && e.isControlDown() && e.getKeyChar() != 's'
+        if (aux.getEditorPane().getEditorPane().isEnabled() && e.isShiftDown() && e.isControlDown() && e.getKeyChar() != 's'
                 && e.getKeyCode() == 83) {
             aux.getSaveFile().saveFileAs(aux.getCurrentFile(), aux.getCurrentFolder(),
                     aux.getEditorPane().getEditorPane().getText());
         }
 
         // Abre um arquivo ao apertar ctrl+o
-        if (aux.getEditorPane().getEditorPane() != null && e.isControlDown() && e.getKeyChar() != 'o' && e.getKeyCode() == 79) {
+        if (aux.getEditorPane().getEditorPane().isEnabled() && e.isControlDown() && e.getKeyChar() != 'o' && e.getKeyCode() == 79) {
             Gui.getInstance().runOpenFile();
         }
 
         // Cria um novo arquivo ao apertar ctr+n
-        if (aux.getEditorPane().getEditorPane() != null && e.isControlDown() && e.getKeyChar() != 'n' && e.getKeyCode() == 78) {
+        if (aux.getEditorPane().getEditorPane().isEnabled() && e.isControlDown() && e.getKeyChar() != 'n' && e.getKeyCode() == 78) {
             Gui.getInstance().runCreateNewFile();
         }
     }
